b659e5bad4d538eac5129d4d3e343cb4763f0335
Isaac Tait <isaac@mountaintopcoding.com>
2023-03-07 14:57:41 -0500

    fix: import netlify-cms 🐛

--- web/src/pages/BlogPage/BlogPage.js
+++ web/src/pages/BlogPage/BlogPage.js
@@ -1,11 +1,16 @@
+import CMS from 'netlify-cms'
+
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
 const BlogPage = () => {
+  // Initialize the CMS object
+  CMS.init()
+  // Now the registry is available via the CMS object.
+  CMS.registerPreviewTemplate('my-template', MyTemplate)
   return (
     <>
       <MetaTags title="Blog" description="Blog page" />
-
       <h1>BlogPage</h1>
       <p>
         Find me in <code>./web/src/pages/BlogPage/BlogPage.js</code>
